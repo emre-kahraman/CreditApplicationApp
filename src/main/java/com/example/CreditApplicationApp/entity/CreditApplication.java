1255e2466509facb57ecd7126b5d56bce6d821f3
emrekahraman <43474063+emre-kahraman@users.noreply.github.com>
2023-02-27 15:10:31 +0300

    Add index

--- src/main/java/com/example/CreditApplicationApp/entity/CreditApplication.java
+++ src/main/java/com/example/CreditApplicationApp/entity/CreditApplication.java
@@ -3,12 +3,18 @@ package com.example.CreditApplicationApp.entity;
 import com.example.CreditApplicationApp.dto.CreditResult;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.stereotype.Indexed;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "credit_application")
+@Table(name = "credit_application", indexes = {
+        @Index(
+                name = "index",
+                columnList = "personalIdentificationNumber, birthDate"
+        )
+})
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
